bfa0bafc1fd009887f9249683a5b92d0f0780f00
TT <tzeting.teow@nintex.com>
2017-09-03 14:45:39 +0800

    add new test

--- TTFunctionalTest.py
+++ TTFunctionalTest.py
@@ -2,24 +2,27 @@ from selenium import webdriver
 import unittest
 import time
 import sys
+from selenium.webdriver.common.keys import Keys
 
 class WebAppTest(unittest.TestCase):
 	def setUp(self):
-		# for diff_browser in diff_Browsers:
-		# 	print (diff_browser)
-		# 	self.browser = diff_browser
-		# self.browser = webdriver.Firefox()
 		self.browser = webdriver.Chrome()
 
 	def tearDown(self):
 		self.browser.quit()
 
 	def test_title_verification(self):
-		self.browser.get('https://docs.djangoproject.com/en/1.11/ref/django-admin/')
-		
-		self.assertIn('Django',self.browser.title)
-		time.sleep(5)
+		self.browser.get('https://www.google.com/')
+		self.assertIn('Google',self.browser.title)
 
+	def test_input_verification(self):
+		self.browser.get('https://www.google.com/')
+		elem = self.browser.find_element_by_name('q')
+		elem.clear()
+		elem.send_keys('Kitty')
+		elem.send_keys(Keys.RETURN)
+		self.assertIn('Kitty',self.browser.title)
+		time.sleep(5)
 
 if __name__ == '__main__':
 	unittest.main()
