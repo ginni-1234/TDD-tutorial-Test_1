03e26811af1b1c0378651a529440e86dedc75bcc
TT <tzeting.teow@nintex.com>
2017-09-09 14:32:45 +0800

    Error handling added in detail page

--- TTProject/TTapp/views.py
+++ TTProject/TTapp/views.py
@@ -2,17 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 def index(request):
     if request.LANGUAGE_CODE =='zh-hans':
         return HttpResponse(request.LANGUAGE_CODE)
     else:
         latest_question_list = Question.objects.order_by('-pub_date')[:5]
-        template = loader.get_template('TTapp/index.html')
-        context = {
-        'latest_question_list': latest_question_list,
-        }
-        return HttpResponse(template.render(context, request))
+        context = {'latest_question_list': latest_question_list}
+        return render(request, 'TTapp/index.html', context)
         #return HttpResponse("You prefer to read another language.")
 
 def TT_Site(request):
@@ -22,7 +20,11 @@ def TT_Site(request):
         return HttpResponse("You prefer to read another language. This is TT site")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'TTapp/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
