d2f1b32d0c20afc65cc9b252f1b747bfbaef67b1
TT <tzeting.teow@nintex.com>
2017-09-09 16:34:06 +0800

    Display choice option in detail page

--- TTProject/TTapp/views.py
+++ TTProject/TTapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 
 def index(request):
     if request.LANGUAGE_CODE =='zh-hans':
@@ -35,4 +36,19 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+            # Redisplay the question voting form.
+            return render(request,'TTapp/detail.html',{
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
